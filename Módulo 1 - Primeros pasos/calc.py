17334717c662607f7807917f8f3e3b1d5de698b1
LeoZCruz <89984631+LeoZCruz@users.noreply.github.com>
2022-02-10 18:17:41 -0600

    Actualizacion implementacion github

---
+++ Módulo 1 - Primeros pasos/calc.py
@@ -0,0 +1,8 @@
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(first_number + second_number)
+
+#Correcion concatenacion
+print(int(first_number) + int(second_number))
+
